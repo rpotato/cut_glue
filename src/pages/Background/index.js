7a719035f5a4f05720929d68827c326371d2eca0
hlli <No8@yeah.net>
2023-10-20 19:41:14 +0800

    split tabs

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,31 @@
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
+
+chrome.action.onClicked.addListener(e => { splitTabs(e) });
+
+async function moveTabsToWin(tabs, win) {
+    if (!!win) {
+        await Promise.all(tabs.map(t => {
+            if (t.id)
+                return chrome.tabs.move(t.id, { windowId: win.id, index: -1 })
+        }));
+    }
+}
+
+async function removeFirstTab(win) {
+    if (!!win) {
+        var d = (await chrome.tabs.query({ windowId: win.id, index: 0 }))[0];
+        d.id && chrome.tabs.remove(d.id)
+    }
+}
+
+async function splitTabs(e) {
+    var tabs = await chrome.tabs.query({ windowId: e.windowId });
+    if (!(tabs.length < 2)) {
+        var o = tabs.filter(i => i.index >= e.index);
+        chrome.windows.create({}, async win => {
+            await moveTabsToWin(o, win);
+            await removeFirstTab(win);
+        })
+    }
+}
